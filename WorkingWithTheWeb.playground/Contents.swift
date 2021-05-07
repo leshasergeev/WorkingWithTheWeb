6d84698fadfb5117e8d38c8fe8f60e77773b0ea8
iBamboola <41113234+iBamboola@users.noreply.github.com>
2021-05-07 22:09:45 +0300

    Was ended

--- WorkingWithTheWeb.playground/Contents.swift
+++ WorkingWithTheWeb.playground/Contents.swift
@@ -15,21 +15,57 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 //task.resume()
 
 
-var urlComponents = URLComponents(string: "https://api.nasa.gov/planetary/apod")!
-urlComponents.queryItems = [
-    "api_key": "DEMO_KEY",
-    "date": "2013-07-16"
-    ].map {URLQueryItem(name: $0.key, value: $0.value)}
-print(urlComponents)
-
-let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, respounce, error) in
-    if let data = data {
-        let str = String(data: data, encoding: .utf8)
-        print(str)
-        PlaygroundPage.current.finishExecution()
+struct PhotoInfo: Codable {
+    var title: String
+    var description: String
+    var url: URL
+    var copyright: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description    = "explanation"
+        case url
+        case copyright
     }
 }
-task.resume()
+
+
+func fetchPhoto(completion: @escaping (Result<PhotoInfo,Error>) -> Void) {
+    
+    var urlComponents = URLComponents(string: "https://api.nasa.gov/planetary/apod")!
+    urlComponents.queryItems = [
+        "api_key":  "DEMO_KEY",
+        "date":     "2013-07-16"
+        ].map {URLQueryItem(name: $0.key, value: $0.value)}
+    print(urlComponents)
+    
+    
+    let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, respounce, error) in
+        let jsonDecoder = JSONDecoder()
+        if let data = data {
+            do {
+                let photoInfo = try jsonDecoder.decode(PhotoInfo.self, from: data)
+                completion(.success(photoInfo))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    task.resume()
+}
+
+fetchPhoto { (result) in
+    
+    switch result {
+    case .failure(let error):
+        print("Fetch Photo Failed with Error: \(error)")
+    case .success(let photoInfo):
+        print("Successfully fetched PhotoInfo: \(photoInfo)")
+    }
+    
+    PlaygroundPage.current.finishExecution()
+}
+
 
 
 
